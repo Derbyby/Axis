2d15f022fbdead32b8a2a7f64614a6ba89bcc895
Eduardo Saucedo <ed.saucedo1020@gmail.com>
2025-12-09 21:57:37 -0600

    Backend funcional: conexion con BD y registro de usuarios

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db'); // Importar conexión a BD
+
+dotenv.config();
+
+connectDB();
+
+//Inicializar la aplicación Express
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Middlewares (Permiten que el servidor entienda datos)
+app.use(express.json()); // Importante: Permite recibir JSON en los POST
+app.use(express.urlencoded({ extended: false }));
+app.use(cors()); // Permite conexiones desde el Frontend (React)
+
+//Importar y Usar las Rutas
+//Aquí conectamos la ruta
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes);
+
+//Arranca el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
